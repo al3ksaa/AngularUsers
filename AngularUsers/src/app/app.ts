ec272d70a08c1dbe044d5e58741ba043be433ece
Nikola Gutić <nikola@scs.co.rs>
2025-10-06 17:08:51 +0200

    "Refactor app structure: Introduced new user management features, updated HTML template, CSS styling, and TypeScript logic for better functionality and maintainability."

--- AngularUsers/src/app/app.ts
+++ AngularUsers/src/app/app.ts
@@ -1,41 +1,49 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { UserService } from './user.service';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {User, UserService} from './user.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './app.html',
+    styleUrls: ['./app.css'],
 })
 export class AppComponent {
-  newName = '';
-  newEmail = '';
-  newAge: number | null = null;
-  newStatus: 'online' | 'offline' = 'offline';
+    newName = '';
+    newEmail = '';
+    newAge: number | null = null;
+    newStatus: 'online' | 'offline' = 'offline';
 
-  constructor(public userService: UserService) {}
+    constructor(public userService: UserService) {
+    }
 
-  addUser() {
-    if (!this.newName || !this.newEmail || this.newAge === null) return;
-    this.userService.addUser({
-      name: this.newName,
-      email: this.newEmail,
-      age: this.newAge,
-      status: this.newStatus,
-    });
-    this.newName = '';
-    this.newEmail = '';
-    this.newAge = null;
-    this.newStatus = 'offline';
-  }
+    addUser() {
+        if (!this.newName || !this.newEmail || this.newAge === null) return;
+        this.userService.addUser({
+            name: this.newName,
+            email: this.newEmail,
+            age: this.newAge,
+            status: this.newStatus,
+        });
+        this.newName = '';
+        this.newEmail = '';
+        this.newAge = null;
+        this.newStatus = 'offline';
+    }
 
-  setAllOnline() {
-    this.userService.setAllStatus('online');
-  }
+    setAllOnline() {
+        this.userService.setAllStatus('online');
+    }
 
-  setAllOffline() {
-    this.userService.setAllStatus('offline');
-  }
+    setAllOffline() {
+        this.userService.setAllStatus('offline');
+    }
+
+    // ovde je castovan status: 'online' | 'offline'
+    protected updateUserStatus(user: User, status: 'online' | 'offline'): void {
+        this.userService.updateUser(user.id, {status})
+        // isto sto i
+        // this.userService.updateUser(user.id, { status: status })
+    }
 }
